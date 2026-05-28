20a8dacb1669f72ed4fd0dac4a983acb514699b7
chrisveni <your-github-janechristopher88@gmail.com>
2026-05-28 17:51:42 +0400

    Heartbeat LED via Kconfig + overlay

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,8 +5,8 @@
 
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
-
+// #define LED_NODE DT_ALIAS(led0)
+#define LED_NODE DT_ALIAS(app_led)//in app.overlay the app-led will change in c to app_led
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
@@ -24,7 +24,8 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS   );
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
+                                                          
