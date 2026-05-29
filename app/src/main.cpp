312a3e3fd9bf0dbd33f46e22ae37db3ea42896e1
chrisveni <your-github-janechristopher88@gmail.com>
2026-05-29 17:12:51 +0400

    Custom Board Copy/Rename method

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,31 +1,14 @@
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
+                                                       /*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-
-
-/* The devicetree node identifier for the "led0" alias. */
-// #define LED_NODE DT_ALIAS(led0)
-#define LED_NODE DT_ALIAS(app_led)//in app.overlay the app-led will change in c to app_led
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+#include <stdio.h>
 
 int main(void)
 {
-    bool led_state = true;
+	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
-    if (!gpio_is_ready_dt(&led)) return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
-
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
-    }
-    return 0;
+	return 0;
 }
-                                                          
